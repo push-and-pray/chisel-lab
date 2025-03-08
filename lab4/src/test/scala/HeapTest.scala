77d779031581f85f146eed73cd7cab8116e29ec6
push-and-pray <62392537+push-and-pray@users.noreply.github.com>
2025-03-08 14:47:47 +0100

    solve lab 4.2

--- lab4/src/test/scala/HeapTest.scala
+++ lab4/src/test/scala/HeapTest.scala
@@ -1,4 +1,3 @@
-
 import heap._
 import Heap.Operation
 import chisel3._
@@ -11,50 +10,126 @@ class HeapTest extends AnyFlatSpec with ChiselScalatestTester {
 
   it should "present the currently largest number on io.root while ready and not empty" in {
     test(new TestHeap) { dut =>
+      for (a <- 1 to 8) {
+        dut.io.op.poke(Operation.Insert)
+        dut.io.newValue.poke(a.U)
+        dut.io.valid.poke(1.B)
+        dut.clock.step()
+        dut.io.valid.poke(0.B)
+        while (!dut.io.ready.peekBoolean()) dut.clock.step()
+        dut.io.root.expect(a.U)
+      }
 
-      // setup a new operation
-      dut.io.op.poke(Operation.Insert)
-      dut.io.newValue.poke(220.U)
-      dut.io.valid.poke(1.B)
-
-      // step to start operation
-      dut.clock.step()
-
-      // the operation has been initiated and we can deassert valid
-      dut.io.valid.poke(0.B)
-
-      // wait for the dut to get ready again
-      while (!dut.io.ready.peekBoolean()) dut.clock.step()
-
-      // the inserted value should appear as the largest value for now
-      dut.io.root.expect(220.U)
-
-      // write more test code here
-
+      for (a <- 8 to 1) {
+        dut.io.op.poke(Operation.RemoveRoot)
+        dut.io.valid.poke(1.B)
+        dut.clock.step()
+        dut.io.valid.poke(0.B)
+        while (!dut.io.ready.peekBoolean()) dut.clock.step()
+        dut.io.root.expect(a.U)
+      }
     }
   }
 
   it should "assert empty after all numbers have been removed" in {
     test(new TestHeap) { dut =>
-      // write your test code here
+      // new heap is empty
+      dut.io.empty.expect(true.B)
+
+      // fill up heap
+      for (a <- 1 to 8) {
+        dut.io.op.poke(Operation.Insert)
+        dut.io.newValue.poke(a.U)
+        dut.io.valid.poke(1.B)
+        dut.clock.step()
+        dut.io.valid.poke(0.B)
+        while (!dut.io.ready.peekBoolean()) dut.clock.step()
+        dut.io.empty.expect(false.B)
+      }
+
+      // empty heap
+      for (a <- 1 to 8) {
+        dut.io.op.poke(Operation.RemoveRoot)
+        dut.io.valid.poke(1.B)
+        dut.clock.step()
+        dut.io.valid.poke(0.B)
+        while (!dut.io.ready.peekBoolean()) dut.clock.step()
+      }
+
+      dut.io.empty.expect(true.B)
+
     }
   }
 
   it should "assert full when 8 numbers have been inserted" in {
     test(new TestHeap) { dut =>
-      // write your test code here
+      // fill up heap
+      for (a <- 1 to 7) {
+        dut.io.op.poke(Operation.Insert)
+        dut.io.newValue.poke(a.U)
+        dut.io.valid.poke(1.B)
+        dut.clock.step()
+        dut.io.valid.poke(0.B)
+        while (!dut.io.ready.peekBoolean()) dut.clock.step()
+        dut.io.full.expect(false.B)
+      }
+
+      dut.io.op.poke(Operation.Insert)
+      dut.io.newValue.poke(8.U)
+      dut.io.valid.poke(1.B)
+      dut.clock.step()
+      dut.io.valid.poke(0.B)
+      while (!dut.io.ready.peekBoolean()) dut.clock.step()
+      dut.io.full.expect(true.B)
     }
   }
 
   it should "deassert full after one number is removed when it was full" in {
     test(new TestHeap) { dut =>
-      // write your test code here
+      // fill up heap
+      for (a <- 1 to 8) {
+        dut.io.op.poke(Operation.Insert)
+        dut.io.newValue.poke(a.U)
+        dut.io.valid.poke(1.B)
+        dut.clock.step()
+        dut.io.valid.poke(0.B)
+        while (!dut.io.ready.peekBoolean()) dut.clock.step()
+      }
+      // remove val
+      dut.io.full.expect(true.B)
+      dut.io.op.poke(Operation.RemoveRoot)
+      dut.io.valid.poke(1.B)
+      dut.clock.step()
+      dut.io.valid.poke(0.B)
+      while (!dut.io.ready.peekBoolean()) dut.clock.step()
+      dut.io.full.expect(false.B)
+
     }
   }
 
   it should "not change the sequence if new insertions are issued when it is full" in {
     test(new TestHeap) { dut =>
-      // write your test code here
+      // fill up heap
+      for (a <- 1 to 8) {
+        dut.io.op.poke(Operation.Insert)
+        dut.io.newValue.poke(a.U)
+        dut.io.valid.poke(1.B)
+        dut.clock.step()
+        dut.io.valid.poke(0.B)
+        while (!dut.io.ready.peekBoolean()) dut.clock.step()
+      }
+      dut.io.full.expect(true.B)
+      dut.io.root.expect(8)
+
+      dut.io.op.poke(Operation.Insert)
+      dut.io.newValue.poke(9.U)
+      dut.io.valid.poke(1.B)
+      dut.clock.step()
+      dut.io.valid.poke(0.B)
+      while (!dut.io.ready.peekBoolean()) dut.clock.step()
+      dut.io.full.expect(true.B)
+      dut.io.root.expect(8)
+
     }
   }
 
