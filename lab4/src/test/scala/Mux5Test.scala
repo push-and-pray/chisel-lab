2a0b63b44b8e3e2cd5219d9efa8243f7d5b30f7c
push-and-pray <62392537+push-and-pray@users.noreply.github.com>
2025-03-08 14:06:20 +0100

    solve lab 4.1

--- lab4/src/test/scala/Mux5Test.scala
+++ lab4/src/test/scala/Mux5Test.scala
@@ -5,7 +5,31 @@ import org.scalatest.flatspec.AnyFlatSpec
 class Mux5Test extends AnyFlatSpec with ChiselScalatestTester {
   "Mux5 " should "pass" in {
     test(new Mux5()) { dut =>
-      // write your test here
+      dut.io.a.poke(4.U)
+      dut.io.b.poke(3.U)
+      dut.io.c.poke(2.U)
+      dut.io.d.poke(1.U)
+      dut.io.e.poke(0.U)
+
+      dut.io.sel.poke(0.U)
+      dut.clock.step()
+      dut.io.y.expect(4.U)
+
+      dut.io.sel.poke(1.U)
+      dut.clock.step()
+      dut.io.y.expect(3.U)
+
+      dut.io.sel.poke(2.U)
+      dut.clock.step()
+      dut.io.y.expect(2.U)
+
+      dut.io.sel.poke(3.U)
+      dut.clock.step()
+      dut.io.y.expect(1.U)
+
+      dut.io.sel.poke(4.U)
+      dut.clock.step()
+      dut.io.y.expect(0.U)
     }
   }
 }
